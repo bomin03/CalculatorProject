dbe7ce67d7f6138b2f65b3817963c9c4afcd2830
kimbomin <bomin0322s2@gmail.com>
2026-06-12 10:02:39 +0900

    입력

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -5,38 +5,37 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        int num1 = scanner.nextInt();
 
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        int num2 = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in); // Scanner, Calculator는 한번만 있어도 되기 때문에  While문 밖에 존재
+        Calculator calculator = new Calculator(); // calculate 메서드를 사용하기 위해 객체를 생성하고 계속 사용하기 때문에 while문 밖에 작성
+        while (true) {
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            int num1 = scanner.nextInt();
 
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        String operator = scanner.next();
+            System.out.println("두 번째 숫자를 입력하세요: "); // 새로운 숫자를 매번 입력받기 위해 while문 안에 존재
+            int num2 = scanner.nextInt();
 
-        int result = 0;
-        switch (operator) {
-            case "+":
-                result = num1 + num2;
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            String operator = scanner.next();
+
+            int result = calculator.calculate(num1, num2, operator); // calculator.calculate()를 호출해 계산 결과를 반환받아 result에 저장
+                                                                     // 매번 새로운 결과값을 받기 위해 while문 안에 존재
+
+            System.out.println("입력한 숫자: " + num1 + ", " + num2); // 입력받은 num1, num2를 콘솔에 출력
+            System.out.println("입력한 연산자: " + operator); // 입력받은 연산자를 콘솔에 출력
+            System.out.println("결과 " + result); // 입력받은 num1, num2와 연산자의 결과를 출력
+            System.out.println("첫 번째 결과를 삭제하시겠습니까? (yes 입력 시 삭제)"); // 연산이 끝난 후에 삭제 여부 확인
+            String deleteInput = scanner.next();
+            // 사용자가 입력한 값을 deleteInput 변수에 저장
+            if (deleteInput.equals("yes")) {
+                calculator.removeResult(); // yes 입력 시 calculator의 results에서 첫 번째(0) 데이터 삭제
+            }
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)"); // 다른 숫자로 더 계산할지 여부 확인
+            String input = scanner.next(); // 사용자가 exit를 입력하면 scanner.next()가 읽고 input 변수에 exit 저장
+            if (input.equals("exit")) { // exit 입력 시 종료
                 break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                if (num2 == 0) {
-                    System.out.println("0으로 나눌 수 없습니다.");
-                } else {
-                    result = num1 / num2;
-                }
-                break;
+
+            }
         }
-        System.out.println("입력한 숫자: " + num1 + ", " + num2);
-        System.out.println("입력한 연산자: " + operator);
-        System.out.println("결과 " + result);
-
     }
 }
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,8 +1,40 @@
 package com.example.calculator;
 
-public class Calculator {
+import java.util.ArrayList;
 
-    public static void main(String[] args) {
-        System.out.println("Hello Calculator!");
+public class Calculator {
+    private ArrayList<Integer> results = new ArrayList<>(); // 연산 결과값을 제한없이 저장하기 위해 ArrayList 사용
+    // 캡슐화를 위해 외부 접근이 불가하도록 private 추가
+    public ArrayList<Integer>getResults() { // getter 메서드
+        return results;
     }
-}
+    public void setResults(ArrayList<Integer>results) { // setter 메서드
+        this.results = results; // 매개변수로 받은 결과 값을 필드 results에 저장 그리고 this가 가리키는 건 Calculator 클래스 자신
+    }
+    public void removeResult() { // 가장 먼저 저장된 결과를 삭제하는 메서드
+        results.remove(0); // ArrayList의 0번째 데이터 삭제
+    }
+    public int calculate (int num1, int num2, String operator) {
+        // num1, num2, operator를 매개변수로 받아 사칙연산을 수행하고 결과값을 반환하는 메서드
+        int result = 0; // 결과값을 저장할 변수, switch문 실행 전 초기값을 0으로 지정
+        switch (operator) { // operator의 값에 따라 결과값을 결정
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) { // 0으로 나눌 수 없음을 의미
+                    System.out.println("0으로 나눌 수 없습니다.");
+                } else {
+                    result = num1 / num2; // num2가 0이 아닐 때 나눗셈 실행
+                }
+                break;
+        } results.add(result); // 계산 결과를 ArrayList에 저장
+        return result; // 연산한 결과값을 호출한 곳으로 돌려주는 역할
+    }
+} // switch문에서 계산 -> result에 저장 -> return으로 반환
